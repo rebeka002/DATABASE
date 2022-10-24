0908f8fe85a8a35c3a488d7217effcfa60859feb
rebeka002 <114580734+rebeka002@users.noreply.github.com>
2022-10-24 17:56:24 +0200

    Add files via upload

---
+++ gym_database_postgresql.sql
@@ -0,0 +1,13 @@
+-- Database: gym
+
+-- DROP DATABASE IF EXISTS gym;
+
+CREATE DATABASE gym
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United Kingdom.1250'
+    LC_CTYPE = 'English_United Kingdom.1250'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
